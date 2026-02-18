0b4de5b7800a0c9b8199a431ef9ec98f37552889
XiaoWang0923 <XiaoWangTongxue0923@outlook.com>
2026-02-18 21:22:47 +0800

    better update func

--- src/update.js
+++ src/update.js
@@ -1,12 +1,45 @@
 const { autoUpdater } = require("electron-updater");
-const { ipcMain } = require("electron");
+const { ipcMain, dialog } = require("electron");
 
-autoUpdater.autoDownload = true;
+// autoUpdater.forceDevUpdateConfig = true;
+autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = true;
 
-function initUpdater() {
+function initUpdater(mainWindow) {
     ipcMain.on("checkUpdate", () => {
-        autoUpdater.checkForUpdatesAndNotify();
+        autoUpdater.checkForUpdates();
+    });
+    autoUpdater.on("update-available", (info) => {
+        dialog
+            .showMessageBox(mainWindow, {
+                type: "info",
+                title: "发现新版本",
+                message: `新版本${info.version}已发布, 是否下载?`,
+                buttons: ["下载", "稍后"],
+            })
+            .then(({ response }) => {
+                if (response === 0) autoUpdater.downloadUpdate();
+            });
+    });
+    autoUpdater.on("update-not-available", () => {
+        dialog.showMessageBox(mainWindow, {
+            type: "info",
+            title: "未发现新版本",
+            message: "暂未发现新版本哦",
+            buttons: ["确定"],
+        });
+    });
+    autoUpdater.on("update-downloaded", () => {
+        dialog
+            .showMessageBox(mainWindow, {
+                type: "info",
+                title: "更新就绪",
+                message: "新版本已下载, 是否立即重启并安装?",
+                buttons: ["安装", "稍后"],
+            })
+            .then(({ response }) => {
+                if (response === 0) autoUpdater.quitAndInstall();
+            });
     });
 }
 
