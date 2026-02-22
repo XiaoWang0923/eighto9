8ac4f075f73ad4541388cbd3ac9de7ef0aee7dae
XiaoWang0923 <XiaoWangTongxue0923@outlook.com>
2026-02-22 17:31:29 +0800

    func not finished yet

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require("electron");
 const path = require("path");
 const update = require(path.join(__dirname, "update.js"));
+// const autopoweroff = require(path.join(__dirname, "autopoweroff.js"));
+const settings = require(path.join(__dirname, "settings.js"));
 
+let mainWindow = null;
 let aboutWindow = null; // 用于防止多个aboutWindow造成openDevtools错误
+let appTray = null;
 
-function createMainWindow() {
-    const mainWindow = new BrowserWindow({
+function createMainWindow(isNotHidden) {
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         titleBarStyle: "hidden",
@@ -18,7 +22,9 @@ function createMainWindow() {
             preload: path.join(__dirname, "/preload/main.js"),
         },
         backgroundColor: "#000000",
+        show: false,
     });
+
     ipcMain.on("openDevtoolsOnMain", () => {
         mainWindow.webContents.openDevTools();
     });
@@ -28,6 +34,9 @@ function createMainWindow() {
     update.initUpdater();
 
     mainWindow.loadURL(path.join(__dirname, "/html/main.html"));
+    if (isNotHidden) {
+        mainWindow.show();
+    }
 }
 
 ipcMain.handle("getVersions", () => {
@@ -59,24 +68,58 @@ function createAboutWindow() {
         },
         backgroundColor: "#000000",
     });
+
     ipcMain.on("openDevtoolsOnAbout", () => {
         aboutWindow.webContents.openDevTools();
     });
+
     aboutWindow.loadURL(path.join(__dirname, "/html/about.html"));
 }
 
-app.whenReady().then(() => {
-    createMainWindow();
-
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createMainWindow();
-        }
+function createAppTray() {
+    const contextMenu = Menu.buildFromTemplate([
+        {label:"设置"},
+        {
+            label: "关于",
+            click: () => {
+                createAboutWindow();
+            },
+        },
+        { type: "separator" },
+        {
+            label: "关闭",
+            click: () => {
+                app.quit();
+            },
+        },
+    ]);
+    appTray = new Tray(path.join(__dirname, "assets/icon.ico"));
+    appTray.setToolTip("EightO9");
+    appTray.setContextMenu(contextMenu);
+    appTray.on("click", () => {
+        mainWindow.isDestroyed() ? createMainWindow(true) : mainWindow.focus();
     });
+}
+
+app.whenReady().then(() => {
+    settings.initSettings().then(settings.readSettings());
+
+    if (process.argv.includes("--hidden")) {
+        createMainWindow(false);
+    } else {
+        createMainWindow(true);
+    }
+
+    createAppTray();
+
+    if (settings.get("main", "autoStart")) {
+        app.setLoginItemSettings({
+            openAtLogin: true,
+            args: ["--hidden"],
+        });
+    }
 });
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+    console.log("main.js: window all closed");
 });
---
+++ src/settings.js
@@ -0,0 +1,71 @@
+const { app } = require("electron");
+const path = require("path");
+const fs = require("fs");
+
+const userDataPath = app.getPath("userData");
+const settingsPath = path.join(userDataPath, "data", "settings.json");
+let settings = null;
+
+async function initSettings() {
+    try {
+        await fs.promises.mkdir(path.dirname(settingsPath), {
+            recursive: true,
+        });
+
+        try {
+            await fs.promises.access(settingsPath);
+        } catch {
+            const defaultSettings = {
+                main: {
+                    autoStart: true
+                },
+                autopoweroff: {
+                    enable: true,
+                    time: ["12:30", "22:15"],
+                },
+            };
+            await fs.promises.writeFile(
+                settingsPath,
+                JSON.stringify(defaultSettings, null, 4),
+            );
+            console.log(
+                "settings.js: setting.json has been created.",
+                settingsPath,
+            );
+        }
+    } catch (e) {
+        console.error("settings.js: initSettings() failed", e);
+    }
+}
+
+function readSettings() {
+    try {
+        settings = JSON.parse(fs.readFileSync(settingsPath, "utf-8"));
+    } catch (e) {
+        console.error("settings.js: readSettings() failed", e);
+    }
+}
+
+async function writeSettings() {
+    try {
+        await fs.promises.writeFile(settingsPath, JSON.stringify(settings));
+    } catch (e) {
+        console.error("settings.js: writeSettings() failed", e);
+    }
+}
+
+function get(module, key) {
+    return settings[module][key];
+}
+
+function set(module, key, value) {
+    settings[module][key] = value;
+    writeSettings(settings);
+}
+
+module.exports = {
+    initSettings,
+    readSettings,
+    get,
+    set,
+};
